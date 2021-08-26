f658437694c4551b21af6d35648ce8db8ec6ca43
tetsuya_kojima <gangelion8@gmail.com>
2021-08-26 21:32:12 +0900

    create a Home mock
    ホーム画面のモックを作成

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-4xl">
+    <div class="bg-blue-200 h-40"></div>
+    <div class="flex h-screen">
+      <div class="w-1/4 text-center bg-blue-200"></div>
+      <div class="w-2/4 text-center">
+        <div class="text-center text-9xl mt-20">English App</div>
+        <div class="text-center mt-20">
+          Menu
+        </div>
+        <div class="flex">
+          <a href="" class="m-4 w-1/2 text-center text-white ml-20 bg-blue-300 rounded-md">Quiz</a>
+          <a href="" class="m-4 w-1/2 text-center text-white mr-20 bg-blue-300 rounded-md">right</a>
+        </div>
+      </div>
+      <div class="w-1/4 text-center bg-blue-200"></div>
+    </div>
+    <div class="bg-yellow-900 h-screen"></div>
+  </div>
+</template>

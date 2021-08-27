761865fcc800a948e3e6f1495482023a563ca4a5
tetsuya_kojima <gangelion8@gmail.com>
2021-08-27 23:56:17 +0900

    Install vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
   <div id="app">
-    <Home />
+    <router-view></router-view>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import Home from "./views/Home.vue"
 
 @Component({
   components: {
-    Home
   },
 })
 export default class App extends Vue {}
